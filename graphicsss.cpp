bdc7d40d977bcbc1f759c55345437d6e0f76c684
Octaviotastico <octaviopercivaldi2@gmail.com>
2021-05-28 00:45:29 -0300

    Added chaikinsAlgorithm, getVectorPercentage and scale2D functions

--- graphicsss.cpp
+++ graphicsss.cpp
@@ -51,7 +51,7 @@ void drawLine(Vertex* v, int x1, int y1, int x2, int y2) {
 }
 
 // Generates a curve given 3 points using an affine combination
-void drawAffineCombinationP3(VertexArray* v, Vector2f p1, Vector2f p2, Vector2f p3, unsigned int it) {
+void drawAffineCombinationP3(VertexArray* v, Vector2f p1, Vector2f p2, Vector2f p3, unsigned int it, bool rewrite = false) {
   double t, t1, t2, t3;
   for (unsigned int i = 0; i < it + 1; i++) {
     t = (double)i / it;
@@ -59,27 +59,23 @@ void drawAffineCombinationP3(VertexArray* v, Vector2f p1, Vector2f p2, Vector2f 
     t2 = (2 * t * (1 - t));
     t3 = (t * t);
 
-    // This is the same, but I changed it because
-    // previously, I needed to set the length of the
-    // VertexArray when creating it. Instead of:
-    // VertexArray curveArray(LineStrip);
-    // I needed to add a second argument like this
-    // VertexArray curveArray(LineStrip, 124);
+    if (rewrite) {
+      (*v)[i].position = Vector2f(
+        (t1 * p1.x) + (t2 * p2.x) + (t3 * p3.x),
+        (t1 * p1.y) + (t2 * p2.y) + (t3 * p3.y)
+      );
+    } else {
+      (*v).append(Vertex(Vector2f(
+        (t1 * p1.x) + (t2 * p2.x) + (t3 * p3.x),
+        (t1 * p1.y) + (t2 * p2.y) + (t3 * p3.y)
+      )));
+    }
 
-    // (*v)[i].position = Vector2f(
-    //   (t1 * p1.x) + (t2 * p2.x) + (t3 * p3.x),
-    //   (t1 * p1.y) + (t2 * p2.y) + (t3 * p3.y)
-    // );
-
-    (*v).append(Vertex(Vector2f(
-      (t1 * p1.x) + (t2 * p2.x) + (t3 * p3.x),
-      (t1 * p1.y) + (t2 * p2.y) + (t3 * p3.y)
-    )));
   }
 }
 
 // Generates a curve given 4 points using an affine combination
-void drawAffineCombinationP4(VertexArray* v, Vector2f p1, Vector2f p2, Vector2f p3, Vector2f p4, unsigned int it) {
+void drawAffineCombinationP4(VertexArray* v, Vector2f p1, Vector2f p2, Vector2f p3, Vector2f p4, unsigned int it, bool rewrite = false) {
   double t, t1, t2, t3, t4;
   for (unsigned int i = 0; i < it + 1; i++) {
     t = (double)i / it;
@@ -88,18 +84,41 @@ void drawAffineCombinationP4(VertexArray* v, Vector2f p1, Vector2f p2, Vector2f 
     t3 = (3 * (t * t) * (1 - t));
     t4 = (t * t * t);
 
-    (*v).append(Vertex(Vector2f(
-      (t1 * p1.x) + (t2 * p2.x) + (t3 * p3.x) + (t4 * p4.x),
-      (t1 * p1.y) + (t2 * p2.y) + (t3 * p3.y) + (t4 * p4.y)
-    )));
+    if (rewrite) {
+      (*v)[i].position = (Vector2f(
+        (t1 * p1.x) + (t2 * p2.x) + (t3 * p3.x) + (t4 * p4.x),
+        (t1 * p1.y) + (t2 * p2.y) + (t3 * p3.y) + (t4 * p4.y)
+      ));
+    } else {
+      (*v).append(Vertex(Vector2f(
+        (t1 * p1.x) + (t2 * p2.x) + (t3 * p3.x) + (t4 * p4.x),
+        (t1 * p1.y) + (t2 * p2.y) + (t3 * p3.y) + (t4 * p4.y)
+      )));
+    }
   }
 }
 
+Vector2f getVectorPercentage(Vector2f v1, Vector2f v2, double p) {
+  return (p * (v2 - v1)) + v1;
+}
+
+// Need to test thissss
 void chaikinsAlgorithm(VertexArray* v, unsigned int it) {
-  return;
+  for (unsigned int i = 0; i < it; i++) {
+    size_t v_sz = (*v).getVertexCount();
+    VertexArray v_aux(LineStrip, v_sx * 2);
+    for(unsigned int i = 0; i < v_sx - 1; i++) {
+      v_aux.append(Vertex(getVectorPercentage((*v)[i], (*v)[i+1], 0.2)));
+      v_aux.append(Vertex(getVectorPercentage((*v)[i], (*v)[i+1], 0.8)));
+    }
+    v = v_aux;
+  }
 }
 
 void rotate2D(VertexArray* v, double angle) {
+  // Need to delete this matrix, because its
+  // slower (and stupid) to multiply this sh!t
+  // instead of coming up with a better solution
   double RotationMatrix[3][3] = {
     { cos(angle), sin(angle), 0},
     {-sin(angle), cos(angle), 0},
@@ -126,6 +145,22 @@ void translate2D(VertexArray* v, double x_t, double y_t) {
     for(unsigned int j = 0; j < 3; j++) {
       new_x += TranslateMatrix[j][i] * (*v)[i].position.x;
       new_y += TranslateMatrix[j][i] * (*v)[i].position.y;
+    }
+    (*v)[i] = Vertex(Vector2f(new_x, new_y));
+  }
+}
+
+void scale2D(VertexArray* v, double s_x, double s_y) {
+  double ScaleMatrix[3][3] = {
+    { s_x,   0, 0},
+    {   0, s_y, 0},
+    {   0,   0, 1},
+  };
+  for(unsigned int i = 0; i < 3; i++) {
+    double new_x = 0, new_y = 0;
+    for(unsigned int j = 0; j < 3; j++) {
+      new_x += ScaleMatrix[j][i] * (*v)[i].position.x;
+      new_y += ScaleMatrix[j][i] * (*v)[i].position.y;
     }
     (*v)[i] = Vertex(Vector2f(new_x, new_y));
   }
